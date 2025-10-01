da4f343028caca62db5eca24b93b41bae69d76dc
Paki93 <masud.mollah@hotmail.it>
2025-10-01 14:07:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'service_8ibg1aq';
-   const templateID = 'template_1k31g6r';
+    const serviceID = 'service_8ibg1aq';
+    const templateID = 'template_1k31g6r';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
+
